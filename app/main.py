a730d7b3ec4cbecfa1ea7845f6c8724e4452b9ba
Vanquishing1 <dvdmirex@gmail.com>
2025-04-30 14:34:20 +0300

    Исправление согласно спринта 2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
 from app.models import PerevalData
 from app.db_manager import DBManager
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -34,3 +35,61 @@ def submit_data(data: PerevalData):
         return {"status": 500, "message": str(e)}
     finally:
         db.close()
+
+@app.get("/submitData/{pereval_id}")
+def get_pereval(pereval_id: int):
+    db = DBManager()
+    try:
+        pereval = db.get_pereval_by_id(pereval_id)
+        if not pereval:
+            raise HTTPException(status_code=404, detail="Pereval not found")
+        return pereval
+    finally:
+        db.close()
+
+
+@app.patch("/submitData/{pereval_id}")
+def update_pereval(pereval_id: int, data: PerevalData):
+    db = DBManager()
+    try:
+        pereval = db.get_pereval_by_id(pereval_id)
+        if not pereval:
+            return {"state": 0, "message": "Pereval not found"}
+
+        if pereval['status'] != 'new':
+            return {"state": 0, "message": "Only records with status 'new' can be updated"}
+
+        update_result = db.update_pereval(pereval_id, data)
+        if update_result:
+            return {"state": 1, "message": "Pereval updated successfully"}
+        else:
+            return {"state": 0, "message": "Failed to update pereval"}
+
+    except Exception as e:
+        return {"state": 0, "message": str(e)}
+    finally:
+        db.close()
+
+
+@app.get("/submitData/")
+def get_perevals_by_user_email(user__email: str = Query(...)):
+    db = DBManager()
+    try:
+        perevals = db.get_perevals_by_email(user__email)
+        return perevals
+    finally:
+        db.close()
+
+@app.get("/", response_class=HTMLResponse)
+def read_root():
+    return """
+    <html>
+        <head>
+            <title>Pereval Project API</title>
+        </head>
+        <body>
+            <h1>Добро пожаловать в Pereval Project API!</h1>
+            <p>Перейдите в <a href="/docs">Swagger-документацию</a> для работы с API.</p>
+        </body>
+    </html>
+    """
